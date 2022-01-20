bfdbcbb6fdc7fd3d60dc82d471739c9cf0343b63
JoaoRafa19 <joaopedrorafael19@gmail.com>
2022-01-20 19:16:02 -0300

    dependencias configurações do projeto cabeçalho da pagina

---
+++ src/components/header.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+function Header() {
+  
+   const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">Minhas Series</NavbarBrand>
+      <NavbarToggler onClick={toggle}></NavbarToggler>
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink
+              href="/"
+            >Gêneros</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              href="/"
+            >Series</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              href="/"
+            >Filmes</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>);
+
+}
+
+export default Header;
