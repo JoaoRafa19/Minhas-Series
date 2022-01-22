5c4cdcd58f8fdfaf4244310cf07221a5dab09a84
João Pedro <joaopedrorafael19@gmail.com>
2022-01-22 09:15:49 -0300

    Header no app e rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, {useState} from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Generos from "./components/generos";
 import Header from "./components/header";
-
+import Home from "./components/home";
 
 function App() {
   return (
     <div>
-      <Header/>
-      
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/generos" element={<Generos />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
